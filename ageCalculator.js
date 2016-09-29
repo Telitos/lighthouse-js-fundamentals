cf267dd1a5306b05036a20cc6fcc0b3938b5a0c3
Ghislain H <telitos@gmail.com>
2016-09-29 19:39:54 +0000

    Adds conditional exercises

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,6 +1,6 @@
 function ageCalculator(name, yearOfBirth, currentYear) {
-var age = (currentYear - yearOfBirth)
-return name + " is " + age + " years old."
+  var age = (currentYear - yearOfBirth)
+  return name + " is " + age + " years old."
 }
 
 console.log(ageCalculator("Miranda", 1983, 2015));
